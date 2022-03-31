8dcbffd5d3091cb6534b4198d3d6cc4f2b1aee0c
atillatahak <atillatahaa@gmail.com>
2022-04-01 01:49:00 +0300

    deleted comment lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,11 @@ class UsersController < ActionController::Base
   def index; end
 
   def show
-    #  @user = User.find(params[:id])
   end
 
   def list
-    # @user = User.find(params[:id])
-    # @posts = @user.posts
   end
 
   def posts
-    # @user = User.find(params[:id])
-    # @post = Post.find(params[:id])
   end
 end
