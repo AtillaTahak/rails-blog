c2f03b2000c49bef37a5c3c486199a7a1c7774f9
atillatahak <atillatahaa@gmail.com>
2022-04-01 01:52:31 +0300

    fixed linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,9 @@ class UsersController < ActionController::Base
 
   def index; end
 
-  def show
-  end
+  def show; end
 
-  def list
-  end
+  def list; end
 
-  def posts
-  end
+  def posts; end
 end
