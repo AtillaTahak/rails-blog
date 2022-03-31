9eac5351fdb54370dc8436c7dde74fa582d38565
atillatahak <atillatahaa@gmail.com>
2022-04-01 01:38:58 +0300

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ActionController::Base
+    layout 'application'
+    
+    def index
+    end
+
+    def show
+      #  @user = User.find(params[:id])
+    end
+
+    def list
+       # @user = User.find(params[:id])
+       # @posts = @user.posts
+    end
+    
+    def posts
+        # @user = User.find(params[:id])
+        # @post = Post.find(params[:id])
+    end
+
+end
