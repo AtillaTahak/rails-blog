537bfd6c74fac1b09bce88f49b5d58fb8bde7499
atillatahak <atillatahaa@gmail.com>
2022-04-14 00:37:09 +0300

    added other model class and model method

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: :user_id
+    has_many :likes
+    has_many :comments
+    after_save :post_increase_counter
+    def load_last_five_comments
+        comment.order('created_at Desc').limit(5)
+      end
+    
+
+    private
+
+    def post_increase_counter
+        author.increment!(:post_counter)
 end
